8a7e682cff0b7f7016009d2fdf0760611c5cd913
daves <daves@hardwaresoftwaresolutions.com>
2020-05-04 19:00:51 -0400

    Lots of code cleanup

---
+++ coolerchart.cpp
@@ -0,0 +1,6 @@
+#include "coolerchart.h"
+
+CoolerChart::CoolerChart()
+{
+
+}
---
+++ coolerchart.h
@@ -0,0 +1,11 @@
+#ifndef COOLERCHART_H
+#define COOLERCHART_H
+
+
+class CoolerChart
+{
+public:
+    CoolerChart();
+};
+
+#endif // COOLERCHART_H
